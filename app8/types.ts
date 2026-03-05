ae7bc0c6fff5f4bb469fada2711e5afc1af7e84b
muziai <478007631@qq.com>
2026-03-05 10:38:44 +0800

    Delete app8 directory

--- app8/types.ts
+++
@@ -1,5 +0,0 @@
-
-import { AspectRatio, ImageSize, GeneratedImage, GenerationConfig, GrsaiApiResponse } from "../types";
-
-export { AspectRatio, ImageSize };
-export type { GeneratedImage, GenerationConfig, GrsaiApiResponse };
